061aaa6b7fb90ab55c38525c7d06b3a023373d25
krw371 <krw371@nyu.edu>
2019-12-13 11:51:31 -0500

    initial models and simulations

---
+++ simulation_script.R
@@ -0,0 +1,385 @@
+library(tidyverse)
+library(lfe)
+library(lme4)
+library(lmerTest)
+
+# Source data generation process functions
+source("dgp_script.R") 
+
+# Set iteration number for simulations
+iter <- 100
+
+# Set seed for reproducibility
+set.seed(2020)
+
+# BASE SIMULATION #####################################################################################################
+
+## call base dgp and store data frame
+classroom_dat <- class_dat_function(40, 25)
+
+## store SATE and generate data frame for models
+SATE <- mean(classroom_dat$Y1) - mean(classroom_dat$Y0)
+model_stu <- classroom_dat %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest,
+                                      minority, parent.edu, fam.income, freelunch, dist.school.hour, 
+                                      gender, pretest, classid)
+
+#### Initial Run ######################################################################################################
+# Base Linear Regression
+lr.base <- lm(Y_stud ~., data = model_stu[,-13])
+
+# Base - Linear Regression Model Bias
+(lr.base.bias <- lr.base$coefficients[2] - SATE)
+
+# Base w/ Fixed Effects
+fixed.base <- lfe::felm(model_stu$Y_stud ~ model_stu$Z_stud + model_stu$minority + model_stu$parent.edu +
+                          model_stu$fam.income + model_stu$freelunch + model_stu$dist.school.hour + model_stu$gender +
+                          model_stu$pretest | model_stu$classid) 
+
+# note: felm function results are same as linear regression with factor(classid)
+
+# Base - Fixed Effects Model Bias
+(fe.base.bias <- fixed.base$coefficients[1] - SATE)
+
+# Base w/ Random Effects
+random.base <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                                gender + pretest + yearstea + avgtest + teach.edu + (1 | classid), data = model_stu)
+
+# Base - Random Effects Model Bias
+(re.base.bias <- summary(random.base)$coefficients[2,1] - SATE)
+
+#### Base Randomization Distribution ##################################################################################
+
+# Generate empty data frame for results
+base_rd_sim <- data.frame(type = rep(NA, iter*3), coef = rep(NA, iter*3), 
+                          conf_int_low = rep(NA, iter*3), conf_int_high = rep(NA, iter*3))
+
+# Initialize counts which is used for which row to store outputs in
+count <- 1
+
+# For loop for randomization distribution - only alters treatment assigment
+
+for(i in 1:iter){
+  
+  # Randomize treatment
+  base_data <- classroom_dat
+  N <- nrow(base_data)
+  
+  X_stud <- rnorm(N, 0, 1)
+  prob_stud <- inv.logit((X_stud/max(abs(X_stud))) * log(19))
+  Z_stud <- rbinom(N, 1, prob = prob_stud)
+  
+  base_data$Z_stud <- Z_stud
+  base_data$Y_stud <- ifelse(Z_stud == 1, base_data$Y1, base_data$Y0)
+  
+  base_data_model <- base_data %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest, minority, parent.edu, 
+                                          fam.income, freelunch, dist.school.hour, gender, pretest, classid)
+  
+  # Linear Regression Sim
+  j <- count
+  base_rd_sim[j,1] <- "lr"
+  lr_base_sim <- lm(Y_stud ~., data = base_data_model[,-13])
+  base_rd_sim[j,2] <- lr_base_sim$coefficients[2]
+  base_rd_sim[j,3] <- confint(lr_base_sim, 'Z_stud', level = .95)[1,1]
+  base_rd_sim[j,4] <- confint(lr_base_sim, 'Z_stud', level = .95)[1,2]
+  
+  # Fixed Effect Sim
+  j <- count+1
+  base_rd_sim[j,1] <- "fixed"
+  fixed_base_sim <- lfe::felm(base_data_model$Y_stud ~ base_data_model$Z_stud + base_data_model$minority +
+                                base_data_model$parent.edu + base_data_model$fam.income + base_data_model$freelunch +
+                                base_data_model$dist.school.hour + base_data_model$gender + base_data_model$pretest |
+                                base_data_model$classid) 
+  base_rd_sim[j,2] <- fixed_base_sim$coefficients[1]
+  base_rd_sim[j,3] <- confint(fixed_base_sim, 'base_data_model$Z_stud', level = .95)[1,1]
+  base_rd_sim[j,4] <- confint(fixed_base_sim, 'base_data_model$Z_stud', level = .95)[1,2]
+  
+  # Random Effect Sim
+  j <- count+2
+  base_rd_sim[j,1] <- "random"
+  random_base_sim <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                                    gender + pretest + yearstea + avgtest + teach.edu + (1 | classid), 
+                                    data = base_data_model) 
+  base_rd_sim[j,2] <- summary(random_base_sim)$coefficients[2,1]
+  base_rd_sim[j,3] <- confint(random_base_sim, 'Z_stud', level = .95)[1,1]
+  base_rd_sim[j,4] <- confint(random_base_sim, 'Z_stud', level = .95)[1,2]  
+  
+  # Increase count for next set of models
+  count <- j+1
+  # Status iteration print
+  print(i)
+}
+
+# Add SATE to results data frame
+base_rd_sim$SATE <- rep(SATE, nrow(base_rd_sim))
+
+# Histograms for base case randomization distribution
+hist(base_rd_sim$coef[base_rd_sim$type == "lr"], main = "Randomization Distribution - Base (Linear Regression)", xlab = "Treatment Effect", xlim = c(min(base_rd_sim$coef)-.5, max = max(base_rd_sim$coef)+.5))
+abline(v = SATE, col = "red")
+
+hist(base_rd_sim$coef[base_rd_sim$type == "fixed"], main = "Randomization Distribution - Base (Fixed Effects)", xlab = "Treatment Effect", xlim = c(min(base_rd_sim$coef)-.5, max = max(base_rd_sim$coef)+.5))
+abline(v = SATE, col = "red")
+
+hist(base_rd_sim$coef[base_rd_sim$type == "random"], main = "Randomization Distribution - Base (Random Effects)", xlab = "Treatment Effect", xlim = c(min(base_rd_sim$coef)-.5, max = max(base_rd_sim$coef)+.5))
+abline(v = SATE, col = "red")
+
+#### Base Sampling Distribution #######################################################################################
+
+# Generate empty data frame for results
+base_sd_sim <- data.frame(type = rep(NA, iter*3), coef = rep(NA, iter*3), 
+                          conf_int_low = rep(NA, iter*3), conf_int_high = rep(NA, iter*3), SATE = rep(NA, iter*3))
+
+# Initialize counts which is used for which row to store outputs in
+count <- 1
+
+for(i in 1:iter){
+  
+  #Sampling Distribution - resample data
+  base_data <- class_dat_function(40, 25)
+  SATE_sim <- mean(base_data$Y1) - mean(base_data$Y0)
+  base_data_model <- base_data %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest, minority, parent.edu, 
+                                          fam.income, freelunch, dist.school.hour, gender, pretest, classid)
+  
+  #Linear Regression Sim
+  j <- count
+  base_sd_sim[j,1] <- "lr"
+  lr_base_sim <- lm(Y_stud ~., data = base_data_model[,-13])
+  base_sd_sim[j,2] <- lr_base_sim$coefficients[2]
+  base_sd_sim[j,3] <- confint(lr_base_sim, 'Z_stud', level = .95)[1,1]
+  base_sd_sim[j,4] <- confint(lr_base_sim, 'Z_stud', level = .95)[1,2]
+  base_sd_sim[j,5] <- SATE_sim
+  
+  #Fixed Effect Sim
+  j <- count+1
+  base_sd_sim[j,1] <- "fixed"
+  fixed_base_sim <- lfe::felm(base_data_model$Y_stud ~ base_data_model$Z_stud + base_data_model$minority +
+                                base_data_model$parent.edu + base_data_model$fam.income + base_data_model$freelunch +
+                                base_data_model$dist.school.hour + base_data_model$gender + base_data_model$pretest |
+                                base_data_model$classid) 
+  base_sd_sim[j,2] <- fixed_base_sim$coefficients[1]
+  base_sd_sim[j,3] <- confint(fixed_base_sim, 'base_data_model$Z_stud', level = .95)[1,1]
+  base_sd_sim[j,4] <- confint(fixed_base_sim, 'base_data_model$Z_stud', level = .95)[1,2]
+  base_sd_sim[j,5] <- SATE_sim
+  
+  #Random Effect Sim
+  j <- count+2
+  base_sd_sim[j,1] <- "random"
+  random_base_sim <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                                      gender + pretest + yearstea + avgtest + teach.edu + (1 | classid), data = base_data_model) 
+  base_sd_sim[j,2] <- summary(random_base_sim)$coefficients[2,1]
+  base_sd_sim[j,3] <- confint(random_base_sim, 'Z_stud', level = .95)[1,1]
+  base_sd_sim[j,4] <- confint(random_base_sim, 'Z_stud', level = .95)[1,2]  
+  base_sd_sim[j,5] <- SATE_sim
+  
+  count <- j+1
+  print(i)
+}
+
+# Histograms for base case sampling distribution
+hist(base_sd_sim$coef[base_sd_sim$type == "lr"], main = "Sampling Distribution - Base (Linear Regression)", 
+     xlab = "Treatment Effect", xlim = c(min(base_sd_sim$coef)-.5, max = max(base_sd_sim$coef)+.5))
+abline(v = mean(base_sd_sim$SATE), col = "red")
+
+hist(base_sd_sim$coef[base_sd_sim$type == "fixed"], main = "Sampling Distribution - Base (Fixed Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(base_sd_sim$coef)-.5, max = max(base_sd_sim$coef)+.5))
+abline(v = mean(base_sd_sim$SATE), col = "red")
+
+hist(base_sd_sim$coef[base_sd_sim$type == "random"], main = "Sampling Distribution - Base (Random Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(base_sd_sim$coef)-.5, max = max(base_sd_sim$coef)+.5))
+abline(v = mean(base_sd_sim$SATE), col = "red")
+
+ggplot() +
+  geom_histogram(data = base_sd_sim[base_sd_sim$type == "lr",], aes(x = coef), 
+                 color = "blue", fill = "white", alpha = .25) +
+  geom_histogram(data = base_sd_sim[base_sd_sim$type == "fixed",], aes(x = coef), 
+                 color = "green", fill = "white", alpha = .25) +
+  geom_histogram(data = base_sd_sim[base_sd_sim$type == "random",], aes(x = coef), 
+                 color = "red", fill = "white", alpha = .25) +
+  labs(x = "Treatment Effect", y = "Frequency") +
+  geom_vline(xintercept=SATE, color = "black", linetype = "dashed", size = 1.25)
+
+# RANDOM EFFECTS VIOLATION SIMULATION #################################################################################
+
+## call random effects dgp and store data frame
+re_dat <- re_dat_function(40, 25)
+
+## store SATE and generate data frame for models
+SATE_re <- mean(re_dat$Y1) - mean(re_dat$Y0)
+model_stu_re <- re_dat %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest, minority, parent.edu, 
+                                  fam.income, freelunch, dist.school.hour, gender, pretest, classid)
+
+#### Initial Run ######################################################################################################
+
+# Random Effects Violation Linear Regression
+lr.re <- lm(Y_stud ~., data = model_stu_re[,-13])
+
+# Random Effects Violation - Linear Regression Model Bias
+(lr.re.bias <- lr.re$coefficients[2] - SATE_re)
+
+# Random Effects Violation w/ Fixed Effects
+fixed.re <- lfe::felm(model_stu_re$Y_stud ~ model_stu_re$Z_stud + model_stu_re$minority + model_stu_re$parent.edu +
+                        model_stu_re$fam.income + model_stu_re$freelunch + model_stu_re$dist.school.hour +
+                        model_stu_re$gender + model_stu_re$pretest | model_stu_re$classid) 
+
+#Random Effects Violation - Fixed Effects Model Bias
+(fe.re.bias <- fixed.re$coefficients[1] - SATE_re)
+
+#Random Effects Violation w/ Random Effects
+random.re <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                              gender + pretest + yearstea + avgtest + teach.edu + (1 | classid), data = model_stu_re)
+
+#Random Effects Violation - Random Effects Model Bias
+(re.re.bias <- summary(random.re)$coefficients[2,1] - SATE_re)
+
+#### REV Randomization Distribution ###################################################################################
+
+# initialize results data frame
+vre_rd_sim <- data.frame(type = rep(NA, iter*3), coef = rep(NA, iter*3), conf_int_low = rep(NA, iter*3), conf_int_high = rep(NA, iter*3))
+
+# initialize count number for results data frame rows
+count <- 1
+
+for(i in 1:iter){
+  
+  # Randomization Distribution - randomize treatment
+  base_data <- re_dat
+  N <- nrow(base_data)
+  
+  X_stud <- rnorm(N, 0, 1)
+  prob_stud <- inv.logit((X_stud/max(abs(X_stud))) * log(19))
+  Z_stud <- rbinom(N, 1, prob = prob_stud)
+  
+  base_data$Z_stud <- Z_stud
+  base_data$Y_stud <- ifelse(Z_stud == 1, base_data$Y1, base_data$Y0)
+  
+  base_data_model <- base_data %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest, minority, parent.edu, 
+                                          fam.income, freelunch, dist.school.hour, gender, pretest, classid)
+  
+  # Linear Regression Sim
+  j <- count
+  vre_rd_sim[j,1] <- "lr"
+  lr_vre_sim <- lm(Y_stud ~., data = base_data_model[,-13])
+  vre_rd_sim[j,2] <- lr_vre_sim$coefficients[2]
+  vre_rd_sim[j,3] <- confint(lr_vre_sim, 'Z_stud', level = .95)[1,1]
+  vre_rd_sim[j,4] <- confint(lr_vre_sim, 'Z_stud', level = .95)[1,2]
+  
+  # Fixed Effect Sim
+  j <- count+1
+  vre_rd_sim[j,1] <- "fixed"
+  fixed_vre_sim <- lfe::felm(base_data_model$Y_stud ~ base_data_model$Z_stud + base_data_model$minority +
+                               base_data_model$parent.edu + base_data_model$fam.income + base_data_model$freelunch +
+                               base_data_model$dist.school.hour + base_data_model$gender + base_data_model$pretest |
+                               base_data_model$classid) 
+  vre_rd_sim[j,2] <- fixed_vre_sim$coefficients[1]
+  vre_rd_sim[j,3] <- confint(fixed_vre_sim, 'base_data_model$Z_stud', level = .95)[1,1]
+  vre_rd_sim[j,4] <- confint(fixed_vre_sim, 'base_data_model$Z_stud', level = .95)[1,2]
+  
+  # Random Effect Sim
+  j <- count+2
+  vre_rd_sim[j,1] <- "random"
+  random_vre_sim <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                                     gender + pretest + (1 | classid), data = base_data_model) 
+  vre_rd_sim[j,2] <- summary(random_vre_sim)$coefficients[2,1]
+  vre_rd_sim[j,3] <- confint(random_vre_sim, 'Z_stud', level = .95)[1,1]
+  vre_rd_sim[j,4] <- confint(random_vre_sim, 'Z_stud', level = .95)[1,2]  
+  
+  count <- j+1
+  print(i)
+}
+
+# add random effects violation SATE to results data frame
+vre_rd_sim$SATE <- rep(SATE_re, nrow(vre_rd_sim))
+
+
+# histograms for violation of random effects assumption
+## linear regression
+hist(vre_rd_sim$coef[vre_rd_sim$type == "lr"], main = "Randomization Distribution - VRE (Linear Regression)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_rd_sim$coef)-.5, max = max(vre_rd_sim$coef)+.5))
+abline(v = SATE_re, col = "red")
+
+## fixed effects
+hist(vre_rd_sim$coef[vre_rd_sim$type == "fixed"], main = "Randomization Distribution - VRE (Fixed Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_rd_sim$coef)-.5, max = max(vre_rd_sim$coef)+.5))
+abline(v = SATE_re, col = "red")
+
+## random effects
+hist(vre_rd_sim$coef[vre_rd_sim$type == "random"], main = "Randomization Distribution - VRE (Random Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_rd_sim$coef)-.5, max = max(vre_rd_sim$coef)+.5))
+abline(v = SATE_re, col = "red")
+
+#### REV Sampling Distribution ########################################################################################
+
+# initialize results data frame
+vre_sd_sim <- data.frame(type = rep(NA, iter*3), coef = rep(NA, iter*3), conf_int_low = rep(NA, iter*3), conf_int_high = rep(NA, iter*3), SATE = rep(NA, iter*3))
+
+# initialize count number for results data frame rows
+count <- 1
+
+for(i in 1:iter){
+  
+  #Sampling Distribution - resample data
+  base_data <- re_dat_function(40, 25)
+  SATE_sim <- mean(base_data$Y1) - mean(base_data$Y0)
+  base_data_model <- base_data %>% select(Y_stud, Z_stud, yearstea, teach.edu, avgtest, minority, parent.edu, 
+                                          fam.income, freelunch, dist.school.hour, gender, pretest, classid)
+  
+  #Linear Regression Sim
+  j <- count
+  vre_sd_sim[j,1] <- "lr"
+  lr_vre_sim <- lm(Y_stud ~., data = base_data_model[,-13])
+  vre_sd_sim[j,2] <- lr_vre_sim$coefficients[2]
+  vre_sd_sim[j,3] <- confint(lr_vre_sim, 'Z_stud', level = .95)[1,1]
+  vre_sd_sim[j,4] <- confint(lr_vre_sim, 'Z_stud', level = .95)[1,2]
+  vre_sd_sim[j,5] <- SATE_sim
+  
+  #Fixed Effect Sim
+  j <- count+1
+  vre_sd_sim[j,1] <- "fixed"
+  fixed_vre_sim <- lfe::felm(base_data_model$Y_stud ~ base_data_model$Z_stud + base_data_model$minority +
+                               base_data_model$parent.edu + base_data_model$fam.income + base_data_model$freelunch +
+                               base_data_model$dist.school.hour + base_data_model$gender + base_data_model$pretest |
+                               base_data_model$classid) 
+  vre_sd_sim[j,2] <- fixed_vre_sim$coefficients[1]
+  vre_sd_sim[j,3] <- confint(fixed_vre_sim, 'base_data_model$Z_stud', level = .95)[1,1]
+  vre_sd_sim[j,4] <- confint(fixed_vre_sim, 'base_data_model$Z_stud', level = .95)[1,2]
+  vre_sd_sim[j,5] <- SATE_sim
+  
+  #Random Effect Sim
+  j <- count+2
+  vre_sd_sim[j,1] <- "random"
+  random_vre_sim <- lmerTest::lmer(Y_stud ~ Z_stud + minority + parent.edu + fam.income + freelunch + dist.school.hour +
+                                     gender + pretest + (1 | classid), data = base_data_model) 
+  vre_sd_sim[j,2] <- summary(random_vre_sim)$coefficients[2,1]
+  vre_sd_sim[j,3] <- confint(random_vre_sim, 'Z_stud', level = .95)[1,1]
+  vre_sd_sim[j,4] <- confint(random_vre_sim, 'Z_stud', level = .95)[1,2]  
+  vre_sd_sim[j,5] <- SATE_sim
+  
+  count <- j+1
+  print(i)
+}
+
+# histograms for sampling distribution - random effects violation
+## linear regression
+hist(vre_sd_sim$coef[vre_sd_sim$type == "lr"], main = "Sampling Distribution - VRE (Linear Regression)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_sd_sim$coef)-.5, max = max(vre_sd_sim$coef)+.5))
+abline(v = mean(vre_sd_sim$SATE), col = "red")
+
+## fixed effects
+hist(vre_sd_sim$coef[vre_sd_sim$type == "fixed"], main = "Sampling Distribution - VRE (Fixed Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_sd_sim$coef)-.5, max = max(vre_sd_sim$coef)+.5))
+abline(v = mean(vre_sd_sim$SATE), col = "red")
+
+## random effects
+hist(vre_sd_sim$coef[vre_sd_sim$type == "random"], main = "Sampling Distribution - VRE (Random Effects)", 
+     xlab = "Treatment Effect", xlim = c(min(vre_sd_sim$coef)-.5, max = max(vre_sd_sim$coef)+.5))
+abline(v = mean(vre_sd_sim$SATE), col = "red")
+
+ggplot() +
+  geom_histogram(data = vre_sd_sim[vre_sd_sim$type == "lr",], aes(x = coef), color = "blue", fill = "white", alpha = .25) +
+  geom_histogram(data = vre_sd_sim[vre_sd_sim$type == "fixed",], aes(x = coef), color = "green", fill = "white", alpha = .25) +
+  geom_histogram(data = vre_sd_sim[vre_sd_sim$type == "random",], aes(x = coef), color = "red", fill = "white", alpha = .25) +
+  labs(x = "Treatment Effect", y = "Frequency") +
+  geom_vline(xintercept=SATE, color = "black", linetype = "dashed", size = 1.25)
+
+# VIOLATE IGNORABILITY SIMULATION #####################################################################################
+
+
