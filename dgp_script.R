0af96ddda0c9bfe758b5fa5bcbd389e818b51c70
krw371 <krw371@nyu.edu>
2019-12-13 13:28:17 -0500

    avgtea update

--- dgp_script.R
+++ dgp_script.R
@@ -31,7 +31,9 @@ class_dat_function <- function(classrooms, students){
   Z_stud <- rbinom(N, 1, prob = prob_stud)
   
   # random probability of assignment at classroom-level 
-  X_class <- rnorm(classrooms, 0, 1) -.03*as.numeric(names(table(avgtest))) # correlate classroom treatment with classroom level predictor
+  avg_tea <- data.frame(classid = rep(1:classrooms, each = students), avgtest = avgtest)
+  avg_tea <- unique(avg_tea)
+  X_class <- rnorm(classrooms, 0, 1) - (.03*avg_tea$avgtest) # correlate classroom treatment with classroom level predictor
   prob_class <- inv.logit((X_class/max(abs(X_class)))*log(19))
   Z_class <- rbinom(classrooms, 1, prob = prob_class)
   Z_class <- rep(Z_class, each = students)
@@ -79,8 +81,10 @@ re_dat_function <- function(classrooms, students){
   prob_stud <- inv.logit((X_stud/max(abs(X_stud))) * log(19))
   Z_stud <- rbinom(N, 1, prob = prob_stud)
   
-  # random probability of assignment at classroom-level 
-  X_class <- rnorm(classrooms, 0, 1) - 0.1*as.numeric(names(table(avgtest))) # correlate classroom treatment with classroom level predictor
+  # random probability of assignment at classroom-level
+  avg_tea <- data.frame(classid = rep(1:classrooms, each = students), avgtest = avgtest)
+  avg_tea <- unique(avg_tea)
+  X_class <- rnorm(classrooms, 0, 1) - (0.1*avg_tea$avgtest) # correlate classroom treatment with classroom level predictor
   prob_class <- inv.logit((X_class/max(abs(X_class)))*log(19))
   Z_class <- rbinom(classrooms, 1, prob = prob_class)
   Z_class <- rep(Z_class, each = students)
